6310786378e27a5e52ceed7fff7ab7523ab85c05
Dumitru <dima.stratulat99@gmail.com>
2026-05-22 09:38:08 +0100

    579: implemented watcher with on_created handler

---
+++ src/bin/watcher.py
@@ -0,0 +1,38 @@
+import subprocess
+import os
+import time
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class FileWatcher(FileSystemEventHandler):
+    def on_created(self, event):
+        item_name = os.path.basename(event.src_path)
+
+        print(f"Event TYPE : {event.event_type} on '{item_name}'")
+        print("Running Linux command: echo hello")
+
+        subprocess.run(['echo', 'hello'], shell=False)
+
+
+if __name__ == "__main__":
+    FOLDER_TO_WATCH = "."
+
+    event_handler = FileWatcher()
+    # Observer watches the file system
+    observer = Observer()
+    observer.schedule(event_handler, path=FOLDER_TO_WATCH)
+    observer.start()
+
+    print("Watcher started")
+    print("Press CTRL+C to exit \n")
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        print("\n🛑 Watcher stopped.")
+
+    observer.join()
